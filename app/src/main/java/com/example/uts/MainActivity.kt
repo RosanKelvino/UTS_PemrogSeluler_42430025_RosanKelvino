f02f2f6f4c4d6c1203ad7797acc6c79bda94fedb
Rosan Kelvino <rosankelvino@gmail.com>
2026-03-25 14:08:37 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/uts/MainActivity.kt
+++ app/src/main/java/com/example/uts/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.uts // Ganti sesuai nama package Anda
+package com.example.uts 
 
 import android.content.Intent
 import android.os.Bundle
@@ -15,19 +15,14 @@ class MainActivity : AppCompatActivity() {
         val etNamaDosen = findViewById<EditText>(R.id.etNamaDosen)
         val btnMasuk = findViewById<Button>(R.id.btnMasuk)
 
-        // Aksi saat tombol MASUK diklik
         btnMasuk.setOnClickListener {
             val nama = etNamaDosen.text.toString()
 
             if (nama.isNotEmpty()) {
-                // Proses Pindah Halaman menggunakan Intent
-                // Pastikan Anda sudah membuat file SecondActivity.kt
                 val intentPindah = Intent(this, SecondActivity::class.java)
 
-                // Membawa data teks ke halaman selanjutnya
                 intentPindah.putExtra("NAMA_DOSEN", nama)
 
-                // Menjalankan perpindahan halaman
                 startActivity(intentPindah)
             } else {
                 Toast.makeText(this, "Nama Dosen harus diisi!", Toast.LENGTH_SHORT).show()
